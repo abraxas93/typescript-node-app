56ea630845b77a8c8a584baeaf9dbc3cf541d084
abraxas93 <savchyk.a@gmail.com>
2019-06-09 16:49:30 +0300

    basic config env

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var x = 25;
+let y = false;
+const s = 'hello type-script';
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+var x: number = 25;
+let y: boolean = false;
+const s: string = 'hello type-script';
+console.log(s);
