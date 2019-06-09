472c46b1545c77ee380a182348339b9649163c35
abraxas93 <savchyk.a@gmail.com>
2019-06-09 18:06:29 +0300

    blank files

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,2 @@
 "use strict";
-var x = 25;
-let y = false;
-const s = 'hello type-script';
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-var x: number = 25;
-let y: boolean = false;
-const s: string = 'hello type-script';
-console.log(s);
-function fn() {
-var aaa = 23;
-    console.log(aaa);
-}
